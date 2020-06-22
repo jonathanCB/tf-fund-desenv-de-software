dd84bca5fe29ae345d67fcda719144ddbfca9746
Jonathan <jonas.ca2@gmail.com>
2020-06-22 14:44:01 -0300

    Atualização da classe ServicoDeCalculoDosValores. SpringBoot funcionando!

--- src/main/java/com/eventoapp/EventoappApplication.java
+++ src/main/java/com/eventoapp/EventoappApplication.java
@@ -2,10 +2,8 @@ package com.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.eventoapp" })
 public class EventoappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/eventoapp/UseCases/ServicoDeCalculoDosValores.java
+++ src/main/java/com/eventoapp/UseCases/ServicoDeCalculoDosValores.java
@@ -10,43 +10,67 @@ import org.springframework.stereotype.Service;
 public class ServicoDeCalculoDosValores {
     private EventoRepository listaEventos;
     private Desconto desconto;
+    private double valorDesconto;
     private ValoresPorDiasDaSemana valoresDiaSemana;
     private ValoresPorQtdPessoas valoresQtdPessoas;
+    private double valorPorDiasDaSemana;
+    private double valorPorQtdDePessoas;
     private Evento evento;
 
     @Autowired
-    public ServicoDeCalculoDosValores(Desconto desconto, ValoresPorDiasDaSemana valoresDiaSemana,
-            ValoresPorQtdPessoas valoresQtdPessoas, Evento evento) {
-        this.listaEventos = null;
+    public ServicoDeCalculoDosValores(EventoRepository listaEventos, Desconto desconto,
+            ValoresPorDiasDaSemana valoresDiaSemana, ValoresPorQtdPessoas valoresQtdPessoas) {
+        this.listaEventos = listaEventos;
         this.desconto = desconto;
         this.valoresDiaSemana = valoresDiaSemana;
         this.valoresQtdPessoas = valoresQtdPessoas;
-        this.evento = evento;
+        this.evento = null;
+        this.valorPorDiasDaSemana = 0;
+        this.valorPorQtdDePessoas = 0;
+        this.valorDesconto = 0;
     }
 
     public Evento getEvento(long codigo) {
-        evento = listaEventos.findByCodigo(codigo);
-        return evento;
+        Evento newEvento = listaEventos.findByCodigo(codigo);
+        if (newEvento == null) {
+            throw new IllegalArgumentException("Evento nao encontrado.");
+        } else {
+            this.evento = newEvento;
+            return evento;
+        }
     }
 
     public double desconto(long codigo) {
-        if (evento == null || evento.getCodigo() != codigo) {
-            evento = getEvento(codigo);
+        if (this.evento == null || this.evento.getCodigo() != codigo) {
+            this.evento = getEvento(codigo);
+            this.valorDesconto = desconto.desconto(this.evento);
         }
-        return desconto.desconto(evento);
+        return this.valorDesconto;
     }
 
     public double valoresPorDiasDaSemana(long codigo) {
-        if (evento == null || evento.getCodigo() != codigo) {
-            evento = getEvento(codigo);
+        if (this.evento == null || this.evento.getCodigo() != codigo) {
+            this.evento = getEvento(codigo);
+            this.valorPorQtdDePessoas = this.valoresDiaSemana.valor(evento);
         }
-        return valoresDiaSemana.valor(evento);
+        return this.valorPorQtdDePessoas;
     }
 
     public double valorPorQtdDePessoas(long codigo) {
-        if (evento == null || evento.getCodigo() != codigo) {
-            evento = getEvento(codigo);
+        if (this.evento == null || this.evento.getCodigo() != codigo) {
+            this.evento = getEvento(codigo);
+            this.valorPorQtdDePessoas = this.valoresQtdPessoas.valor(evento);
         }
-        return valoresQtdPessoas.valor(evento);
+        return this.valorPorQtdDePessoas;
+    }
+
+    public double getValorTotalEvento() {
+        double valorTotalDoEvento = 0;
+        if (this.valorPorDiasDaSemana > 0 && this.valorPorQtdDePessoas > 0) {
+            valorTotalDoEvento = (this.valorPorDiasDaSemana + this.valorPorQtdDePessoas - this.valorDesconto);
+        } else {
+            return 0;
+        }
+        return valorTotalDoEvento;
     }
 }
