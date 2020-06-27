691b2722d9f09164dc8c91a14bf9a182983cc7b0
Alisson Kissel <alissonkissel@gmail.com>
2020-06-27 17:50:31 -0300

    Remocao dos comentarios do js e html

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,8 +1,5 @@
-// Consulta os dados do aluno
 async function salvarEvento(nomeEvento, diaSemana,
     qtdPessoas, descontoPromo) {
-    //console.log(matricula);
-
     let url = "http://localhost:8080/detalhesevento/persiste";
     url = url + "?nomeEvento=" + nomeEvento;
     url = url + "&qtdPessoas=" + qtdPessoas;
@@ -10,48 +7,16 @@ async function salvarEvento(nomeEvento, diaSemana,
     url = url + "&descPromocional=" + descPromocional;
 
     //let url = "http://localhost:8080/detalhesevento/persiste?nomeEvento=Formatura&qtdPessoas=255&diaSemana=Quarta&descPromocional=sim";
-
     console.log(url);
 
     try {
         let resposta = await fetch(url);
-        console.log(resposta);
-        // if (resposta.ok) {
-        //     let dados = await resposta.json();
-        //     console.log(dados);
-        //     return dados;
-        // } else {
-        //     console.log(resposta.status + ", text=" + resposta.statusText);
-        //     return null;
-        // }
         return resposta;
     } catch (erro) {
         console.log(erro);
     }
 }
 
-// Consulta aprovação do aluno
-// async function consultaAprovacao(matricula) {
-//     let url = "http://localhost:8080/consulta_aluno/aprovacao";
-//     url = url + "?matricula=" + matricula;
-
-//     try {
-//         let resposta = await fetch(url);
-//         console.log(resposta);
-//         if (resposta.ok) {
-//             let dados = await resposta.json();
-//             console.log(dados);
-//             return dados;
-//         } else {
-//             console.log(resposta.status + ", text=" + resposta.statusText);
-//             return null;
-//         }
-//     } catch (erro) {
-//         console.log(erro);
-//     }
-// }
-
-// --- início do programa
 document.getElementById("btnSalvar").onclick = async function () {
     nomeEvento = document.getElementById("nomeEvento").value;
     diaSemana = document.getElementById("diaSemana").value;
@@ -70,45 +35,5 @@ document.getElementById("btnSalvar").onclick = async function () {
         return "Evento adicionado com sucesso";
     }
     return "Aconteceu um erro ao adicionar o evento";
-
-    // if (resposta != null) {
-    //     let nome = document.getElementById("codEventoValor");
-    //     nome.innerHTML = resposta.nome;
-    //     let g1 = document.getElementById("g1Valor");
-    //     g1.innerHTML = resposta.grauG1;
-    //     let g2 = document.getElementById("g2Valor");
-    //     g2.innerHTML = resposta.grauG2;
-    //     erro = document.getElementById("erro");
-    //     erro.innerHTML = "";
-    //     //let json = document.getElementById("jsonValor");
-    //     //json.innerHTML = JSON.stringify(resposta);
-    // } else {
-    //     let nome = document.getElementById("nomeValor");
-    //     nome.innerHTML = " - ";
-    //     let g1 = document.getElementById("g1Valor");
-    //     g1.innerHTML = " - ";
-    //     let g2 = document.getElementById("g2Valor");
-    //     g2.innerHTML = " - ";
-    //     erro = document.getElementById("erro");
-    //     erro.innerHTML = "Erro na consulta dos dados";
-    // }
-
-    // document.getElementById("btAprovacao").onclick = async function () {
-    //     matricula = document.getElementById("matricula").value;
-    //     let resposta = await consultaAprovacao(matricula);
-    //     if (resposta != null) {
-    //         let aprovacao = document.getElementById("aprovacao");
-    //         if (resposta == true) {
-    //             aprovacao.innerHTML = "Aluno matricula: " + matricula + ", APROVADO";
-    //         } else {
-    //             aprovacao.innerHTML = "Aluno matricula: " + matricula + ", REPROVADO";
-    //         }
-    //         erro = document.getElementById("erro");
-    //         erro.innerHTML = " - ";
-    //     } else {
-    //         erro = document.getElementById("erro");
-    //         erro.innerHTML = "Erro na consulta da aprovacao";
-    //     }
-    // }
 }
 
